2f8dd077a57f8eba4e2977e2689fa7fb3ce7b5b7
landycandy <landyrich117@gmail.com>
2015-09-27 01:22:12 -0400

    added orient to wll function

--- wallfollow.m
+++ wallfollow.m
@@ -2,9 +2,14 @@ function  w(serPort)
 
 % vars to track
 followingWall = 0;
-returnedToOrigin = 0;
+haveLeftFirstContact = 0;
+atFirstContact = 0;
 globloc = [0,0,0];
 startloc = [0,0,0];
+
+% robot info
+diameter = .3;
+
 
 % start going forward
 SetFwdVelAngVelCreate(serPort,0.15,0);
@@ -29,6 +34,12 @@ while ~followingWall
     pause(0.2);
 end
 
+orientToWall(serPort, followingWall, globloc);
+
+followWall(serport);
+
+SetFwdVelAngVelCreate(serPort,0.15,0);
+
 % loop forever, will break when reach the starting point
 % while ~returnedToOrigin
 % end
@@ -40,4 +51,63 @@ function [globloc] = updatePosition(globloc, distance, angle)
     globloc(2) = globloc(2) + (distance * sin(globloc(3)));
     globloc(3) = globloc(3) + angle;
     globloc(3) = mod(globloc(3), 2.0*pi);
+end
+
+function [followingWall, globloc] = senseBumperContact(serPort, followingWall, globloc)
+    [ BumpRight, BumpLeft, ~, ~, ~, BumpFront] = BumpsWheelDropsSensorsRoomba(serPort); % Read Bumpers
+    if BumpRight || BumpLeft || BumpFront
+       SetFwdVelAngVelCreate(serPort,0,0);
+       
+    end
+end
+
+function orientToWall(serPort)
+    WallSensor = WallSensorReadRoomba(serPort);
+    display(WallSensor);
+    
+    
+    % Turn left until you find a wall with the sensor
+    while ~WallSensor
+        turnAngle(serPort, 0.2, 5); 
+        WallSensor = WallSensorReadRoomba(serPort);
+        display('seeking wall');
+    end
+
+    WallSensor = WallSensorReadRoomba(serPort);
+    display(WallSensor);
+    display('found wall'); 
+    
+    %Keep Turning Left until there is no longer a wall
+    while WallSensor
+        turnAngle(serPort, 0.2, 5);
+        
+        WallSensor = WallSensorReadRoomba(serPort);      
+        display(WallSensor)
+        display('turning past wall');
+    end
+    
+    WallSensor = WallSensorReadRoomba(serPort);      
+    
+    % now find the second wall contact
+    while ~WallSensor
+        turnAngle(serPort, 0.2, 5); 
+        WallSensor = WallSensorReadRoomba(serPort);
+        display('seeking wall');
+    end
+    
+    WallSensor = WallSensorReadRoomba(serPort);      
+    
+    %do it again for fun, keeping track of the length of your turn
+    turns = 0
+    while WallSensor
+        turnAngle(serPort, 0.2, 5); 
+        WallSensor = WallSensorReadRoomba(serPort);
+        display('seeking wall second contact');
+        turns = turns + 1;
+    end
+    
+    turns = turns/2;
+    turnAngle(serPort, .1, -5 * turns);
+    
+    display('following wall');
 end
